ad7c193916c235626791d00336b52be6dc05aace
mtejada <mtejada@controlla.com.mx>
2021-11-09 15:27:36 -0700

    first commit

---
+++ database/factories/DessertFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Dessert;
+use Faker\Generator as Faker;
+
+$factory->define(Dessert::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
